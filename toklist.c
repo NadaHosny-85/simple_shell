5397115a07614d249643931b8d543b7f37be8c83
Nada Hosny <nadahosni96@gmail.com>
2022-11-16 11:13:42 -0800

    added a file that creates a list of tokens

---
+++ toklist.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * toklist - function creates an array of tokens
+ *
+ * @command: string which represents arguments which will
+ * be passed to the program
+ * @delim: the delimeter that you need to break string
+ *
+ * Return: pointer to the first element in the array
+ */
+char **toklist(char *command, char *delim)
+{
+	char **argv = NULL;
+
+	int count = 0;
+
+	char *bstr = NULL;
+
+	char *token = _strtok(command, delim, &bstr);
+
+	while (token != NULL)
+	{
+		argv = _realloc((*argv) * (count), sizeof(*argv) * (count + 1));
+		if (argv == NULL)
+		{
+			perror("Error: Allocation Error\n");
+			exit(EXIT_FAILURE);
+		}
+		argv[count] = token;
+		token = _strtok(NULL, delim, &bstr);
+		count++;
+	}
+	argv = _realloc(argv, sizeof(*argv) * (count), sizeof(*argv) * (count + 1));
+	argv[count] = NULL;
+	return (argv);
+}
