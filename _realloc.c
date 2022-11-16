b647bde319f495c8b9c5192966406a86cc8ecb15
Nada Hosny <nadahosni96@gmail.com>
2022-11-16 11:01:12 -0800

    added re allocation file

--- _realloc.c
+++ _realloc.c
@@ -1,1 +1,48 @@
 #include "main.h"
+
+/**
+ * _realloc - function re allocates new space
+ * in the memory
+ *
+ *
+ * @ptr: pointer to the variable that needs to be
+ * re-allocated
+ * @cursize: current size of the variable
+ * @newsize: new size of the variable
+ *
+ * Return: a pointer to the newly allocated var
+ */
+void *_realloc(void *ptr, unsigned int cursize, unsigned int newsize)
+{
+	void *tempptr;
+
+	unsigned int i;
+
+	unsigned int min = cursize > newsize ? cursize : newsize;
+
+	if (newsize == cursize)
+	{
+		return (ptr);
+	}
+	if (ptr == NULL)
+	{
+		ptr = malloc(newsize);
+		return (ptr);
+	}
+	if (newsize == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (0);
+	}
+	tempptr = malloc(newsize);
+	if (tempptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < min; i++)
+	{
+		*((char *)tempptr + i) = *((char *)ptr + i);
+	}
+	free(ptr);
+	return (tempptr);
+}
